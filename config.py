ef0e7b8a9949f13912e38e7318553188f35f926d
根号3 <1148695207@qq.com>
2020-09-21 21:09:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import argparse
 
 parser = argparse.ArgumentParser(description='PET Challenge')
 
-# data in/out and dataset
-parser.add_argument('--dataset_train_path', default='./0911/enhancement/train/*/*.png',
+# data in/out and dataset 注意，我们对数据集进行了离线处理。数据集首先经过crop.py文件离线处理好并保存在本地文件夹./dataset/enhancement下
+parser.add_argument('--dataset_train_path', default='./dataset/enhancement/train/*/*.png',
                     help='train root path')
-parser.add_argument('--dataset_test_path', default='./0911/enhancement/test/AD&CN&MCI',
+parser.add_argument('--dataset_test_path', default='./dataset/enhancement/test/AD&CN&MCI',
                     help='test root path')
-parser.add_argument("--save_dir", default='v0916_b7', help="all data dir")
-parser.add_argument('--v', type=str, default='v16',
+parser.add_argument("--save_dir", default='v0920_b8', help="all data dir")
+parser.add_argument('--v', type=str, default='v7',
                     help='version')
-#
+# 图像增强
 parser.add_argument('--ColorJitter', type=float, default=0.5,
                     help='transforms.ColorJitter')
 parser.add_argument('--RandomRotation', type=int, default=180,
@@ -18,11 +18,11 @@ parser.add_argument('--RandomRotation', type=int, default=180,
 parser.add_argument('--Resize', type=int, default=224,
                     help='transforms.Resize')
 # train
-parser.add_argument('--batch_size', type=list, default=16,
+parser.add_argument('--batch_size', type=list, default=10,
                     help='batch size of trainset')
 parser.add_argument('--k', type=int, default=20,
                     help='Cross-validation')
-parser.add_argument('--epochs', type=int, default=20,
+parser.add_argument('--epochs', type=int, default=30,
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='learning rate (default: 0.01)')
@@ -30,7 +30,7 @@ parser.add_argument('--gamma', type=float, default=0.5,
                     help='gamma')
 parser.add_argument('--step_size', type=int, default=4,
                     help='step_size')
-# resume
+# resume 中断训练后恢复训练
 parser.add_argument('--resume', type=bool, default=False,
                     help='resume')
 parser.add_argument('--start_epoch', type=int, default=0,
