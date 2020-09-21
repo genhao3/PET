6963fdd811e03ae59afcef510d9bcd9a93699018
根号3 <1148695207@qq.com>
2020-09-21 21:10:34 +0800

    Update crop.py

--- crop.py
+++ crop.py
@@ -6,49 +6,30 @@ import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 
 
-def second(path, save_path):  # 把第一次裁剪的图片填充为正方形，避免resize时会改变比例
-    i = 0
-    for file in os.listdir(path):
-        i += 1
-        file_path = os.path.join(path, file)
-        new_path = os.path.join(save_path, file)
-        # if i != 50:
-        #     continue
-        print(file_path)
-        img = Image.open(file_path).convert('RGB')
-
-        # tf = transforms.Compose([transforms.RandomAffine(0, scale=(0.8, 0.8))])
-        # img = tf(img)
-        img = np.array(img)
-        # img = img[5:138,15:230]
-        print(img.shape)
-
-        # plt.imshow(img, cmap='Greys_r')
-        # plt.show()
-        # break
-        # 以最长的一边为边长，把短的边补为一样长，做成正方形，避免resize时会改变比例
-        dowm = img.shape[0]
-        up = img.shape[1]
-        max1 = max(dowm, up)
-        dowm = (max1 - dowm) // 2
-        up = (max1 - up) // 2
-        dowm_zuo, dowm_you = dowm, dowm
-        up_zuo, up_you = up, up
-        if (max1 - img.shape[0]) % 2 != 0:
-            dowm_zuo = dowm_zuo + 1
-        if (max1 - img.shape[1]) % 2 != 0:
-            up_zuo = up_zuo + 1
-        matrix_pad = np.pad(img, pad_width=((dowm_zuo, dowm_you),  # 向上填充1个维度，向下填充两个维度
-                                            (up_zuo, up_you),  # 向左填充2个维度，向右填充一个维度
-                                            (0, 0))  # 通道数不填充
-                            , mode="constant",  # 填充模式
-                            constant_values=(0, 0))  # 第一个维度（就是向上和向左）填充6，第二个维度（向下和向右）填充5
-        print(matrix_pad.shape)
-        img = Image.fromarray(matrix_pad)
-        img.save(new_path)
-        # plt.imshow(matrix_pad, cmap='Greys_r')
-        # plt.show()
-        # break
+def second(img, new_path):  # 把第一次裁剪的图片填充为正方形，避免resize时会改变比例
+    img = np.array(img)
+    # img = img[5:138,15:230]
+    print(img.shape)
+    # 以最长的一边为边长，把短的边补为一样长，做成正方形，避免resize时会改变比例
+    dowm = img.shape[0]
+    up = img.shape[1]
+    max1 = max(dowm, up)
+    dowm = (max1 - dowm) // 2
+    up = (max1 - up) // 2
+    dowm_zuo, dowm_you = dowm, dowm
+    up_zuo, up_you = up, up
+    if (max1 - img.shape[0]) % 2 != 0:
+        dowm_zuo = dowm_zuo + 1
+    if (max1 - img.shape[1]) % 2 != 0:
+        up_zuo = up_zuo + 1
+    matrix_pad = np.pad(img, pad_width=((dowm_zuo, dowm_you),  # 向上填充1个维度，向下填充两个维度
+                                        (up_zuo, up_you),  # 向左填充2个维度，向右填充一个维度
+                                        (0, 0))  # 通道数不填充
+                        , mode="constant",  # 填充模式
+                        constant_values=(0, 0))  # 第一个维度（就是向上和向左）填充6，第二个维度（向下和向右）填充5
+    print(matrix_pad.shape)
+    img = Image.fromarray(matrix_pad)
+    img.save(new_path)
 
 
 def first(path, save_path):  # 第一次裁剪大脑
@@ -61,6 +42,7 @@ def first(path, save_path):  # 第一次裁剪大脑
         #     continue
         print(file_path)
         img = Image.open(file_path).convert('RGB')
+        img = np.array(img)
         # print(img.shape)
         index = np.where(img > 50)  # 找出像素值大于50的所以像素值的坐标
         # print(index)
@@ -82,25 +64,12 @@ def first(path, save_path):  # 第一次裁剪大脑
             max_y = img.shape[1]
         if min_y < 0:
             min_y = 0
-        #     # print(min_x, max_x, min_y, max_y)
         img = Image.fromarray(img[min_x:max_x, min_y:max_y, :])
-        #     # plt.imshow(img, cmap='Greys_r')
-        #     # plt.show()
-        #     # p = r'dataset/fusai/enhancement/enhancement/train/AD'
-        # 　new_path = os.path.join(p, file)
-        img.save(new_path)
-    #     break
+
+        second(img, new_path)
 
 
 if __name__ == '__main__':
-    path = r'./dataset/0910/enhancement/test/AD&CN&MCI'
-    save_path = r'./dataset/0911/enhancement/test/AD&CN&MCI'
-    # first(path, save_path)
-    # second(path, save_path)
-    # img = Image.open(path).convert('RGB')
-    # tf = transforms.Compose([ transforms.RandomRotation(degrees=90, expand=True)])
-    # img = tf(img)
-    # img = np.array(img)
-    # print(img.shape)
-    # plt.imshow(img, cmap='Greys_r')
-    # plt.show()
+    path = r'./dataset/train/AD'  # 原始图片路径
+    save_path = r'./dataset/enhancement/train/AD'  # 保存的图片路径
+    first(path, save_path)
